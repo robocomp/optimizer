616b2e969af332d79104a4dad7a06a69997de1b5
pbustos <pbustos@unex.es>
2022-01-31 23:42:01 +0100

    dwa-cpp

---
+++ dwa-cpp/src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Comment out this line if your application has a QtGui
+#define USE_QTGUI
+
+
+#define PROGRAM_NAME    "dwa_cpp"
+#define SERVER_FULL_NAME   "RoboComp dwa_cpp:: dwa_cpp"
+
+#endif
