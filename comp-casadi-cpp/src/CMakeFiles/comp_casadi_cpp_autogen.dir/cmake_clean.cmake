a84150b65acd39bcc80236cf87b8590638bc79f3
pbustos <pbustos@unex.es>
2022-09-10 12:17:49 +0200

    deletein temp files

--- comp-casadi-cpp/src/CMakeFiles/comp_casadi_cpp_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/comp_casadi_cpp_autogen"
-  "comp_casadi_cpp_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/comp_casadi_cpp_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
