c0cabe61039e6adbd2b98a9c9ad7ace25ee4fa4e
pbustos <pbustos@unex.es>
2022-09-10 12:14:49 +0200

    deleting temp files

--- local_grid/src/CMakeFiles/local_grid_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/local_grid_autogen"
-  "local_grid_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/local_grid_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
